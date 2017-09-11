65afb6aaadaf429bd8395de8acc020e1131ac735
jommersu <jommer.su@foxmail.com>
2017-09-11 17:13:42 +0800

    exercise of for loop in chapter5

---
+++ src/exercise_chapter5/greatestCommonDivisor.java
@@ -0,0 +1,29 @@
+package exercise_chapter5;
+
+import java.util.Scanner;
+
+import org.omg.CORBA.portable.ValueBase;
+
+import exercise_chapter3.theChineseZodiac;
+
+public class greatestCommonDivisor {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("input two numbers:");
+		int a = input.nextInt();
+		int b = input.nextInt();
+		
+		int gcd = 1;
+		for(int k = 2; (k <= (a/2) && k <= (b/2) ); k ++) {
+			if(a % k == 0 && b % k == 0) {
+				gcd = k;
+			}
+		}
+		System.out.println("a is " + a
+				+ ". And b is " + b
+				+ ". the gereatest divisor is " + gcd);
+
+	}
+
+}
