1d36d01283af22ea17997d69605d36243afd4a90
jommersu <jommer.su@foxmail.com>
2017-09-13 11:11:29 +0800

    exercise 5-2

---
+++ src/exercise_chapter5/exercise_5_2.java
@@ -0,0 +1,39 @@
+package exercise_chapter5;
+
+import java.util.Scanner;
+
+public class exercise_5_2 {
+
+	public static void main(String[] args) {
+		final int NUMBER_OF_QUIZ = 5;
+		Scanner input = new Scanner(System.in);
+		int count = 0;
+		int correctCount = 0;
+		long stratTime = System.currentTimeMillis();
+		String output = "";
+		
+		while (count < NUMBER_OF_QUIZ) {
+			int a = (int)(1 + Math.random() * 15);
+			int b = (int)(1 + Math.random() * 15);
+			System.out.println("enter the answer of " + a + " + " + b);
+			int answer = input.nextInt();
+			String quizAnswer = "";
+			
+			if(answer == a + b) {
+				quizAnswer = "correct";
+				correctCount++;
+			}else {
+				quizAnswer = "wrong";
+			}
+			
+			count++;
+			output += a + " + " + b + " = " + answer + " " + quizAnswer + "\n";
+		}
+		
+		long endTime = System.currentTimeMillis();
+		System.out.println("The correct number of quiz is " + correctCount
+				+ "\n" + "The test time is " + (endTime - stratTime) / 1000 + "seconds"
+				+ "\n" + output);
+	}
+
+}
