ee7c37d95ac0694ab95900288ae7cb614f854a00
jommersu <jommer.su@foxmail.com>
2017-09-14 11:57:46 +0800

    test

--- src/exercise_chapter6/generateRandomChar.java
+++ src/exercise_chapter6/generateRandomChar.java
@@ -4,7 +4,7 @@ public class generateRandomChar {
 
 	public static void main(String[] args) {
 		for(int i = 0; i < 5; i++) {
-			System.out.println(getRandom('a' , 'z'));
+			System.out.println(getRandom('A' , 'Z'));
 		}
 
 	}
