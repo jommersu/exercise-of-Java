190d9c88b56a51bf5d249e27e58035e010165e5f
jommersu <jommer.su@foxmail.com>
2017-09-14 11:57:03 +0800

    add generate random char

---
+++ src/exercise_chapter6/generateRandomChar.java
@@ -0,0 +1,15 @@
+package exercise_chapter6;
+
+public class generateRandomChar {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i++) {
+			System.out.println(getRandom('a' , 'z'));
+		}
+
+	}
+	
+	public static char getRandom(char ch1, char ch2) {
+		return (char)(ch1 + Math.random() * (ch2 - ch1 + 1));
+	}
+}
