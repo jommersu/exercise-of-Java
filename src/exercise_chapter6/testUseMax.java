f50da21ea8fd2c19108536dddd616d0ee51e5ee7
jommersu <jommer.su@foxmail.com>
2017-09-14 11:08:32 +0800

    add use getMax

---
+++ src/exercise_chapter6/testUseMax.java
@@ -0,0 +1,12 @@
+package exercise_chapter6;
+
+public class testUseMax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 8;
+		int b = 6;
+		System.out.println(getMax.max(a, b));
+	}
+ 
+}
