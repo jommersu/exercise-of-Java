41909eee7d27ed48fe7a5bd55504344b5dd34ad2
jommersu <jommer.su@foxmail.com>
2017-09-20 15:22:05 +0800

    add copyarray

--- src/exercise_chapter7/copyArray.java
+++ src/exercise_chapter7/copyArray.java
@@ -3,28 +3,26 @@ package exercise_chapter7;
 public class copyArray {
 
 	public static void main(String[] args) {
-		int[] myArray = new int[5];
-		System.out.println("source Array is : ");
-		for(int i = 0; i < myArray.length; i++) {
+		int n = 5;
+		int[] myArray = new int[n];
+		for(int i = 0; i < n; i++) {
 			myArray[i] = (int)(Math.random() * 10);
-			System.out.print(myArray[i] + " ");
 		}
-		
-		System.out.println("\nthe copy array use for loop is :");
-		int[] array2 = new int[5];
-		for(int i = 0; i < array2.length; i++) {
-			array2[i] = myArray[i];
-			System.out.print(array2[i] + " ");
-		}
-		
+		System.out.println("source Array is : ");
+		printArray(myArray);
+				
 		System.out.println("\nthe copy array use arraycopy is :");
 		int[] array3 = new int[5];
-		System.arraycopy(array2, 0, array3, 0, array2.length);
-		for(int i = 0; i < array3.length; i++) {
-			System.out.print(array3[i] +" ");
-		}
-		
+		System.arraycopy(myArray, 0, array3, 0, myArray.length);
+		printArray(array3);
 
 	}
-
+	
+	public static void printArray(int[] array) {
+		for(int i = 0; i < array.length; i++) {
+			System.out.print(array[i] +" ");
+		}
+		System.out.println("\n");
+	}
+	
 }
