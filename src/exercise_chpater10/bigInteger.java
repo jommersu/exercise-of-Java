245cea71b23d30b03bbf61bcfbb29f289854226b
jommersu <jommer.su@foxmail.com>
2017-09-28 10:01:01 +0800

    add BitInteger

---
+++ src/exercise_chpater10/bigInteger.java
@@ -0,0 +1,22 @@
+package exercise_chpater10;
+
+import java.math.BigInteger;
+
+public class bigInteger {
+
+	public static void main(String[] args) {
+		System.out.println("50! is \n" +
+					factorial(50));
+
+	}
+	
+	public static BigInteger factorial(long n) { 
+		BigInteger result = BigInteger.ONE;
+		
+		for(int i = 1; i <= n; i++) {
+			result = result.multiply(new BigInteger(i + ""));
+		}
+		return result;
+	}
+
+}
